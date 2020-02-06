f442960dc5703b469841fa01822f694d41c741cf
Celinemmbonekerrine4 <celinekerrine@gmail.com>
2020-02-06 10:03:52 +0300

    it will add the API keys for the news

--- app/config.py
+++ app/config.py
@@ -1,6 +1,15 @@
 class Config:
-    NEWS_API_BASE_URL='https://newsapi.org/v2/everything?q=bitcoin&from=2020-01-01&sortBy=publishedAt&apiKey=cd2297074fc14b4ca59f3b4f3135f6c2'
+    NEWS_API_KEY = 'cd2297074fc14b4ca59f3b4f3135f6c2'
+
+    NEWS_API_BASE_URL = 'https://newsapi.org/v2/sources?sortBy=publishedAt&apiKey={}'
+
+    ARTICLE_API_BASE_URL = "https://newsapi.org/v2/everything?sources={}&apiKey={}"
+
+
 class prodConfig(Config):
     pass
+
+
+
 class DevConfig(Config):
     DEBUG = True
